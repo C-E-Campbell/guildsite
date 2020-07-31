aa21f4fe26d8bde086fe9eb884476d8b2980e7b3
Charles <codelyfewithcharlie@gmail.com>
2020-07-31 03:52:04 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import Landing from './pages/landing';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Apply from './pages/Apply';
+import FAQ from './pages/FAQ';
 
 function App() {
-  return <Landing />;
+  return (
+    <>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route exact path="/apply">
+          <Apply />
+        </Route>
+        <Route exact path="/faq">
+          <FAQ />
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import style from './navigation.module.css';
+export default function Nav() {
+  return (
+    <div className={style.container}>
+      <div className={style.nav_container}>
+        <NavLink
+          activeClassName={style.selected}
+          className={style.navlink}
+          exact
+          to="/"
+        >
+          HOME
+        </NavLink>
+        <NavLink
+          className={style.navlink}
+          activeClassName={style.selected}
+          exact
+          to="/faq"
+        >
+          FAQ
+        </NavLink>
+        <NavLink
+          className={style.navlink}
+          activeClassName={style.selected}
+          exact
+          to="/apply"
+        >
+          APPLY
+        </NavLink>
+      </div>
+    </div>
+  );
+}
