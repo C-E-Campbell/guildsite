181b7e8cd57920bd1e2fb730dd467260660aea4d
Charles <codelyfewithcharlie@gmail.com>
2020-07-31 04:16:32 -0400

    updates

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -5,17 +5,27 @@ import logo from '../assets/images/logo.png';
 
 export default function landing() {
   return (
-    <div className={style.container}>
-      <div className={style.landing_container}>
-        <img data-aos="fade-in" alt="logo" className={style.logo} src={logo} />
-        <h1 data-aos="fade-in">Apostles of the Abyss</h1>
+    <>
+      <div className={style.container}>
+        <div className={style.landing_container}>
+          <img
+            data-aos="fade-in"
+            alt="logo"
+            className={style.logo}
+            src={logo}
+          />
+          <h1 className={style.headline} data-aos="fade-in">
+            Apostles of the Abyss
+          </h1>
+        </div>
+        <div className={style.overlay}></div>
+        <div className={style.video_container}>
+          <video className={style.video} autoPlay muted loop>
+            <source src={video} type="video/mp4" />
+          </video>
+        </div>
       </div>
-      <div className={style.overlay}></div>
-      <div className={style.video_container}>
-        <video className={style.video} autoPlay muted loop>
-          <source src={video} type="video/mp4" />
-        </video>
-      </div>
-    </div>
+      <h2>Guild History</h2>
+    </>
   );
 }
