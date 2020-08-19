347b5665394c835f150eb651be7e0d5eaae697b3
Charles <codelyfewithcharlie@gmail.com>
2020-08-19 16:17:32 -0400

    updates

--- src/pages/PreSiteVideo.js
+++ src/pages/PreSiteVideo.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import style from './PreSiteVideo.module.css';
-import video from '../assets/video/fire.mp4';
+import video from '../assets/video/newFire.mp4';
 import Logo from '../assets/images/logo.png';
 import audio from '../assets/music/HeroesOfOId-Mix1.mp3';
 function PreSiteVideo(props) {
@@ -20,14 +20,14 @@ function PreSiteVideo(props) {
 
   return (
     <div className={style.video_container}>
-      <audio className="audio-element">
+      <audio className='audio-element'>
         <source src={audio} loop />
       </audio>
 
-      <img className={style.logo} src={Logo} alt="aota" />
+      <img className={style.logo} src={Logo} alt='aota' />
       <div className={style.overlay}></div>
       <video className={style.video} autoPlay muted loop>
-        <source src={video} type="video/mp4" />
+        <source src={video} type='video/mp4' />
       </video>
       <button onClick={goToSite} className={style.button}>
         Enter
@@ -42,12 +42,12 @@ function PreSiteVideo(props) {
         {playing ? (
           <div>
             <span className={style.soundSpan}>Mute</span>
-            <i className="fa fa-volume-off fa-sm " aria-hidden="true"></i>
+            <i className='fa fa-volume-off fa-sm ' aria-hidden='true'></i>
           </div>
         ) : (
           <div>
             <span className={style.soundSpan}>Unmute</span>
-            <i className="fa fa-volume-up fa-sm" aria-hidden="true"></i>
+            <i className='fa fa-volume-up fa-sm' aria-hidden='true'></i>
           </div>
         )}
       </button>
