0ea6da1a9f036a446ac79f1d4f76099d4a8c5fd9
Charles <codelyfewithcharlie@gmail.com>
2020-08-19 15:25:57 -0400

    updates

--- src/pages/Plans.js
+++ src/pages/Plans.js
@@ -5,27 +5,27 @@ export default function Plans() {
   return (
     <div className={styles.container}>
       <FaqQuestion
-        question="Have we decided on a node area / cluster?
-"
-        answer="Same as above answer, these talks will remain private, but to expand on these issues as you’ll continue to see ~ We want to run a diplomatic economy. In that respect, we are occasionally running polls to gauge what the majority of our players are interested in and building some discussions based on those results.
-
-To get best, most accurate results, we want to push for 125 ~ 150/+ and run a few key polls, have a couple of targeted discussions, & make some decisions together.?
-"
+        question='Why are we being so secretive?
+'
+        answer='Because we don’t want others to interfere with our plans, just as we aren’t scheming against anyone else. There is also just so much we’re still learning about the game, and things we just don’t know yet. It’s difficult to fully develop ideas until a more complete database exists for a knowledge-base.
+'
       />
       <FaqQuestion
-        question="Will we be streaming anything?
-"
-        answer="We have several members that will be streaming parts of Alpha, Beta, & launch. We have an official Twitter & Twitch:
+        question='Have we decided on a node area / cluster?
+'
+        answer='Same as above answer, these talks will remain private, but to expand on these issues as you’ll continue to see ~ We want to run a diplomatic economy. In that respect, we are occasionally running polls to gauge what the majority of our players are interested in and building some discussions based on those results.
+
+To get best, most accurate results, we want to push for 125 ~ 150/+ and run a few key polls, have a couple of targeted discussions, & make some decisions together.
+'
+      />
+      <FaqQuestion
+        question='Will we be streaming anything?
+'
+        answer='We have several members that will be streaming parts of Alpha, Beta, & launch. We have an official Twitter & Twitch:
 
 Twitter: https://twitter.com/AbyssApostles &
 Twitch: https://www.twitch.tv/apostlesoftheabyss
-"
-      />
-      <FaqQuestion
-        question="Why are we being so secretive?
-"
-        answer="Because we don’t want others to interfere with our plans, just as we aren’t scheming against anyone else. There is also just so much we’re still learning about the game, and things we just don’t know yet. It’s difficult to fully develop ideas until a more complete database exists for a knowledge-base.
-"
+'
       />
     </div>
   );
--- src/pages/RequirementsFaq.js
+++ src/pages/RequirementsFaq.js
@@ -5,39 +5,39 @@ export default function RequirementsFaq(props) {
   return (
     <div className={styles.container}>
       <FaqQuestion
-        question="What are the requirements to join Apostles of the Abyss?"
-        answer="We’re looking for players that are motivated and driven
+        question='What are the requirements to join Apostles of the Abyss?'
+        answer='We’re looking for players that are motivated and driven
 You should be planning to play & be active within our community by launch time at least 4 evenings a week during our Prime-Time.
 You must have a headset to listen in for team communication via Discord. Mic required only for leadership & officer roles, but strongly requested.
-"
+'
       />
       <FaqQuestion
-        question="Will your guild be active in the Alpha/Beta tests?"
-        answer="There are a couple of members with Alpha-1 access, few members that will have Alpha-2 access, and a decent amount of out members will have beta access. While access is not required to join, we are hoping to have 40-60%/+ of our members active in the beta. We will also have several people dedicated to streaming Alpha / Beta for our members to watch & learn from.
-"
+        question='Will your guild be active in the Alpha/Beta tests?'
+        answer='There are a couple of members with Alpha-1 access, few members that will have Alpha-2 access, and a decent amount of our members will have beta access. While access is not required to join, we are hoping to have 40-60%/+ of our members active in the beta. We will also have several people dedicated to streaming Alpha / Beta for our members to watch & learn from.
+'
       />
       <FaqQuestion
-        question="Will we be active before the release of Ashes of Creation?"
-        answer="Yes, keep reading. But also ~ We don’t want to just be a reservation spot for you, although we can be. But we want you to be invested in reserving your spot *and* playing to grow with us. If we have synergy on launch, we will kick ass. More on this in the Launch-Plans Topic Q&A.
-"
+        question='Will we be active before the release of Ashes of Creation?'
+        answer='Yes, keep reading. But also ~ We don’t want to just be a reservation spot for you, although we can be. But we want you to be invested in reserving your spot and playing to grow with us. If we have synergy on launch, we will dominate. 
+'
       />
       <FaqQuestion
-        question="What will be our most active time of day?"
-        answer="6 PM until 12 ~ 1 A.M. Sunday - Thursday &
-    5 PM until 2 A.M. Friday & Saturday"
+        question='What will be our most active time of day?'
+        answer='6 PM until 12 ~ 1 A.M. Sunday - Thursday &
+    5 PM until 2 A.M. Friday & Saturday'
       />
       <FaqQuestion
-        question="What activity will get me kicked or suspended?"
-        answer="Being inactive for over 30 days with no notice or reasonable explanation.
+        question='What activity will get me kicked or suspended?'
+        answer='Being inactive for over 30 days with no notice or reasonable explanation.
 Being blatantly awful to another Guild-mate based on case.
 Exploiting / using any cheats in an MMO environment.
 Leaking of private information to outside sources.
 Streaming during vulnerable events without leadership permission.
 Streaming without guild-mate notice or permission to be included.
-"
+'
       />
       <FaqQuestion
-        question="Is your community currently playing any games together before AoC launches?"
+        question='Is your community currently playing any games together before AoC launches?'
         answer="While we wait on Ashes of Creation, strategy... Playing with us in key titles we organize should be indicative of how we will operate in Ashes of Creation~ Helping others, creating guides & FAQ's frequently updated, indexes for helping you succeed faster, and working together to move forward. We felt Final Fantasy XI promoted many of Ashes of Creation’s key qualities requiring cooperation with others within its own mechanics.
 
         Serpico & Kino will co-head the open-invite (probably Saturday) nights in/after October for Star Wars: Squadrons. He'll also be the leader of the Last Epoch Division. 
