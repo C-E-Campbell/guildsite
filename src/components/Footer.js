55626e5e776de9e7363e09cd9f290bdf8cb3a123
Charles <codelyfewithcharlie@gmail.com>
2020-08-21 02:06:43 -0400

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,19 +5,19 @@ export default function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.innerContainer}>
-        <div className={styles.box}>
+        <div className={styles.textBox}>
           <h3 className={styles.h3}>Apostles of the Abyss</h3>
           <h4 className={styles.h4}>An Ashes of Creation Guild</h4>
           <img className={styles.logo} src={logo} />
         </div>
         <div className={styles.box}>
           <iframe
-            src="https://discordapp.com/widget?id=735220428397609023&theme=dark"
-            width="100%"
-            height="100%"
-            allowtransparency="true"
-            frameborder="0"
-            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+            src='https://discordapp.com/widget?id=735220428397609023&theme=dark'
+            width='100%'
+            height='100%'
+            allowtransparency='true'
+            frameborder='0'
+            sandbox='allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts'
           ></iframe>
         </div>
       </div>
